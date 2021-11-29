089e68df744fdb95ceee6f37ba2f125a0f489c18
suyeon <sroovy@naver.com>
2021-11-29 22:39:54 +0900

    WordRelay 함수형 Component ( useCallback )

---
+++ WordRelay.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { useState, useRef, useCallback } from 'react';
+
+const WordRelay = () => {
+  const [ word, setWord ] = useState('스루비');
+  const [ value, setValue ] = useState('');
+  const [ result, setResult ] = useState('');
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  const onSubmitForm = useCallback<(e:React.FormEvent) => void>((e) => {
+    e.preventDefault();
+    const input = inputEl.current;
+
+    if(word[word.length - 1] === value[0]) {
+      setResult('Good!');
+      setWord(value);
+      setValue('');
+      if(input) {
+        input.focus();
+      }
+    } else {
+      setResult('bad!');
+      setValue('');
+      if(input) {
+        input.focus();
+      }
+    }
+  }, [word, value]);
+
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value)
+  }, [])
+
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input 
+          ref={inputEl}
+          value={value}
+          onChange={onChange}
+        />
+        <button>입력</button>
+      </form>
+      <div>{result}</div>
+    </>
+  )
+}
+
+export default WordRelay;
--- client.tsx
+++ client.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader/root';
 
 import GuGuDan from './GuGuDan';
+import WordRelay from './WordRelay';
 
-ReactDOM.render(<GuGuDan />, document.querySelector('#root'));
+const Hot = hot(WordRelay); // HOC
+
+ReactDOM.render(<Hot />, document.querySelector('#root'));
